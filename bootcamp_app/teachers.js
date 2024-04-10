9fdeca913af2b6535a3b3c848be9f7fb8dfb8228
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-10 19:10:08 -0400

    Refactor SQL queries in students.js and teachers.js to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,25 +7,27 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2]
+const values = [`${cohortName || 'JUL02'}`]
 
-
-pool.query(`
-  SELECT
-    teachers.name AS teacher,
-    cohorts.name AS cohort
-  FROM
-    teachers
-    JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-    JOIN students ON students.id = assistance_requests.student_id
-    JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE
-    cohorts.name LIKE '%${process.argv[2]}%'
-  GROUP BY
-    teachers.name,
-    cohorts.name
-  ORDER BY
-    teachers.name;
-  `)
+const queryString = `
+SELECT
+  teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM
+  teachers
+  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE
+  cohorts.name LIKE $1
+GROUP BY
+  teachers.name,
+  cohorts.name
+ORDER BY
+  teachers.name;
+`
+pool.query(queryString, values)
   .then((res) => {
     res.rows.forEach((teacher) => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
